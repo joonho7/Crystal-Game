c9f9b89285f78efd6bdaf9be6c05e61b15ecf0ee
Joonho Lee <joonholee@Joonhos-MacBook-Air.local>
2017-12-12 20:08:36 -0500

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,27 @@
-window.onload = function(){
-$("#crystal-1").click(crystalGenerator('x'));
-$("#crystal-2").click(crystalGenerator('y'));
-$("#crystal-3").click(crystalGenerator('z'));
-$("#crystal-4").click(crystalGenerator('w'));
-$("#goalNumber").html(goalNumberGen);
-gameGoing=true;
-};
-
 var crystalOne = 'x';
 var crystalTwo = 'y';
 var crystalThree = 'z';
 var crystalFour = 'w';
-var goalNumberGen = goalNumberGenerator();
 var totalNumber;
 var gameGoing = false;
 var win;
 var losses;
-function crystalGenerator(x){
-  return x = Math.floor(Math.random()*12+1);
+function crystalGenerator(a){
+  return a = Math.floor(Math.random()*12+1);
 }
 
 function goalNumberGenerator(){
 	return Math.floor(Math.random()*120+19)
 }
+
+window.onload = function(){
+$("#crystal-1").click(crystalGenerator('x'));
+$("#crystal-2").click(crystalGenerator('y'));
+$("#crystal-3").click(crystalGenerator('z'));
+$("#crystal-4").click(crystalGenerator('w'));
+$("#goalNumber").html(goalNumberGenerator());
+gameGoing=true;
+};
 
 function initializeGame(){
 if(gameGoing){
@@ -30,7 +29,7 @@ $("#crystal-1").click(crystalGenerator('x'));
 $("#crystal-2").click(crystalGenerator('y'));
 $("#crystal-3").click(crystalGenerator('z'));
 $("#crystal-4").click(crystalGenerator('w'));
-$("#goalNumber").html(goalNumberGen);
+$("#goalNumber").html(goalNumberGenerator());
 gameGoing = true;
  }
  addtition();
@@ -42,7 +41,7 @@ function addition(){
 	$("#crystal-1").click('y');
 	$("#crystal-1").click('z');
 	$("#crystal-1").click('w');
-	totalNumber = 'x' + 'y' + 'z' + 'w';
+	totalNumber = 'x' + 'y'+ 'z' + 'w';
 	$("#totalNumber").html(totalNumber);
  }
 }
